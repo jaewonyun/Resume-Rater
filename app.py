854c80445a9753e11c914a25790f89b831f72523
John Feng <johnfengphd@gmail.com>
2023-07-09 13:17:12 -0400

    added full resume expander

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import streamlit as st
-from app_functions import get_matches_resume
+import openai
+import pandas as pd
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from app_functions import get_matches_resume, show_resume
+
+
+embedding_function = OpenAIEmbeddings()
+
+resumedb = Chroma(persist_directory="chroma/full_resume/",
+                    collection_name="resume_full",
+                    embedding_function=embedding_function)
 
 # openApiKey = st.secrets["OPENAI_API_KEY"]
 # openai.api_key = openApiKey
 
 st.title("Resume Rater")
+
+show_resume = st.radio('show full resume', ['yes', 'no'])
 
 with st.sidebar:
     # st.image(image, width=100)
@@ -13,10 +26,20 @@ with st.sidebar:
     matchtype = st.radio('Select match type', ['resume', 'skills', 'work experience'])
     top_k = st.slider('Select the number of candidates to return', 1, 20, 10)
 
-user_input = st.text_input('Enter the description of perfect candidate for hire')
+user_input = st.text_area('Enter the description of perfect candidate for hire')
 st.write('You entered: ', user_input)
+
 matches = get_matches_resume(user_input, k=top_k, match_type=matchtype)
 
 st.dataframe(matches[['full_name', 'distance']])
+
+matches_name = [name for name in matches['full_name'].values]
+# print(matches_name)
+for i, name in enumerate(matches_name):
+    resume_expander = st.expander(f"See full resume of {name}", expanded=False)
+    # print(name)
+    # print(type(name))
+    full_resume = resumedb.get(where={"full_name": name})['documents'][0]
+    resume_expander.write(full_resume)
 
 
--- app_functions.py
+++ app_functions.py
@@ -9,12 +9,13 @@ import streamlit as st
 # openai.api_key = OPENAI_API_KEY
 
 openApiKey = st.secrets["OPENAI_API_KEY"]
-print(openApiKey)
+# print(openApiKey)
 openai.api_key = openApiKey
 
 embedding_function = OpenAIEmbeddings()
 
 def get_matches_resume(query, k=10, match_type="resume"):
+
     if match_type == "work":
         db = Chroma(persist_directory="chroma/work/",
                 collection_name="resume_work",
@@ -35,5 +36,13 @@ def get_matches_resume(query, k=10, match_type="resume"):
     content = [result[0].page_content for result in results]
     df = pd.DataFrame({"full_name": full_name, "distance": distance, "content": content})   
     return df
+
+resumedb = Chroma(persist_directory="chroma/full_resume/",
+                    collection_name="resume_full",
+                    embedding_function=embedding_function)
+
+def show_resume(full_name):
+    full_resume = resumedb.get(where={"full_name": full_name})['documents'][0]
+    return full_resume
 
 
